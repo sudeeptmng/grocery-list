34f891175dc267fc4141414649ed0ccbe44f2738
sudip <sktamangpersonal@gmail.com>
2026-01-29 14:38:39 +0545

    linking js and index.html

---
+++ js/app.js
@@ -0,0 +1,12 @@
+import { groceryItems } from "./data.js";
+import { createItems } from "./items.js";
+
+let items = groceryItems;
+function render() {
+  const app = document.getElementById("app");
+  app.innerHTML = "";
+
+  const itemsElement = createItems(items);
+  app.appendChild(itemsElement);
+}
+render();
