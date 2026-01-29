25134c73ff545aa8388157d737140eff1ca522a7
sudip <sktamangpersonal@gmail.com>
2026-01-29 09:33:08 +0545

    Creating single item ui

---
+++ js/single-item.js
@@ -0,0 +1,19 @@
+export function createSingleItem(item) {
+  const div = document.createElement("div");
+  div.className = "single-item";
+
+  div.innerHTML = `
+    <input type="checkbox" ${item.completed ? "checked" : ""} />
+    <p style="text-decoration: ${item.completed ? "line-through" : "none"}">
+      ${item.name}
+    </p>
+    <button class="btn icon-btn edit-btn" type="button">
+      <i class="fa-regular fa-pen-to-square"></i>
+    </button>
+    <button class="btn icon-btn remove-btn" type="button">
+      <i class="fa-regular fa-trash-can"></i>
+    </button>
+  `;
+
+  return div;
+}
